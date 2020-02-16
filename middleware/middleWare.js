70240c5a6b6cf1612153b497107e8110abc392c2
adirsam14 <adir.samuel16@gmail.com>
2020-02-16 16:32:52 +0200

    feat: Added getAttribute func

--- middleware/middleWare.js
+++ middleware/middleWare.js
@@ -2,24 +2,39 @@ const ldap = require("ldapjs");
 const urlencode = require("urlencode");
 const PresenceFilter = require("ldapjs").PresenceFilter;
 
+
+function getAttribute(req){
+  const attrib = {};
+  if(req.filter.filters){
+    req.filter.filters.forEach(filter => {
+      attrib[filter.attribute] = filter.value || filter.initial
+    });
+  }
+  else{
+    attrib[req.filter.attribute] = req.filter.value || req.filter.initial;
+  }
+  return attrib;
+}
+
 function urlEncoded(req, res, next) {
   const urlAttribute = new PresenceFilter({
     attribute: "url"
   });
-
-  if (urlAttribute.matches({[req.filter.attribute]: req.filter.value || req.filter.initial})) 
+  const attrib = getAttribute(req);
+  if (urlAttribute.matches(attrib)) 
     {
+      console.log("asd");
     const response = {
       dn: "o=example",
       attributes: {
-        url: req.filter.value || req.filter.initial,
-        urlencoded: urlencode(req.filter.value || req.filter.initial)
+        url: attrib.url,
+        urlencoded: urlencode(attrib.url)
       }
     };
     res.send(response);
   } 
   else {
-    return next(new ldap.NoSuchAttributeError(req.filter.attribute.toString()));
+    return next(new ldap.NoSuchAttributeError(Object.keys(attrib).toString()));
   }
 
   res.end();
@@ -27,18 +42,17 @@ function urlEncoded(req, res, next) {
 }
 
 function findIfUserExist(req,res,next){
-    const urlAttribute = new PresenceFilter({
-        attribute: "userId",
-        attribute:"userName"
-    });
-    const users = {adir:1111,
-                    dor:2222,
-                    liad:333};
-    console.log(req.filter.matches(urlAttribute));
+
+    const users = [{userName:"adir",userId:1111},
+                    {userName:"dor",userId:2222},
+                    {userName:"liad",userId:333}];
     
-    if (urlAttribute.matches(req.filter)) {
-        console.log("asd");
-    }
+    users.forEach(user => {
+      console.log(user);
+      if(req.filter.matches(user)){
+        console.log(user);
+      }
+    });
     res.end();
     return next();
 }
